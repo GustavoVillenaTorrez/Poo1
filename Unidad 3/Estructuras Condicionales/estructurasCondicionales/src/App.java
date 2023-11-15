ef6c38436cd358394213d22573caa49e5b0fefca
GustavoVillenaTorrez <51249576+GustavoVillenaTorrez@users.noreply.github.com>
2023-11-15 02:42:13 -0300

    EJ:Estructura Secuencial
    Ejemplo de Estructura Secuencial Utilizando Visual Code

---
+++ Unidad 3/Estructuras Condicionales/estructurasCondicionales/src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+         //declaración de variables
+         int n1, n2;
+         Scanner sc = new Scanner(System.in);
+         //leer el primer número 
+         System.out.println("Introduce un número entero: ");                                                       
+         n1 = sc.nextInt();      //lee un entero por teclado
+         //leer el segundo número
+         System.out.println("Introduce otro número entero: "); 
+         n2 = sc.nextInt();      //lee un entero por teclado    
+         //mostrar resultado
+         System.out.println("Ha introducido los números: " + n1 + " y " + n2); 
+    }
+}
